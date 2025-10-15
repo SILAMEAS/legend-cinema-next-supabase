9c5d81a346506334587effe33bd468908da66b38
Sila MEAS <sila.meas@allweb.com.kh>
2025-10-15 17:19:32 +0700

    still work

--- utils/api/__movie.ts
+++ utils/api/__movie.ts
@@ -1,26 +1,26 @@
 import {EnumTableName} from "@/utils/enum/EnumTable";
 import {_tb_movie} from "@/utils/api/supabase_tb/_tb_movie";
 import {_gets} from "@/utils/api/__general";
-import {_checkRole} from "@/utils/api/_getCurrentUser";
 import {EnumTableColum} from "@/utils/enum/EnumTableColum";
 
 export async function _getMovies() {
     // const {isAdmin} = await _checkRole();
-    // const select=[
-    //     EnumTableColum.ID,
-    //     EnumTableColum.TITLE,
-    //     EnumTableColum.IMAGE,
-    //     EnumTableColum.RATING,
-    //     EnumTableColum.DURATION,
-    //     EnumTableColum.GENRE,
-    //     `status:MovieStatus(id,name)`
-    // ]
-    // if(isAdmin){
-    //     select.push(EnumTableColum.RELEASE_DATE)
-    // }
+    const select = [
+        EnumTableColum.ID,
+        EnumTableColum.TITLE,
+        EnumTableColum.IMAGE,
+        EnumTableColum.RATING,
+        EnumTableColum.DURATION,
+        EnumTableColum.GENRE,
+        `status:MovieStatus(id,name)`
+    ]
+    if (true) {
+        select.push(EnumTableColum.RELEASE_DATE)
+    }
     // Now fetch movies
     const movies = await _gets<Array<_tb_movie>>({
         tableName: EnumTableName.Movie,
+        select: select.join(",")
     });
     console.log(movies.data[0]);
     return movies;
